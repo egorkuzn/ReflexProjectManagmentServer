17aeabad4fc69f555f3c5b77a009003872474d36
egorkuzn <egorkuzn2002@mail.ru>
2024-03-09 21:43:00 +0700

    project init

---
+++ ride-users/src/main/kotlin/ru/nsu/fit/reflex/project_management/ride_users/CoreApplication.kt
@@ -0,0 +1,11 @@
+package ru.nsu.fit.reflex.project_management.ride_users
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoreApplication
+
+fun main(args: Array<String>) {
+    runApplication<CoreApplication>(*args)
+}
---
+++ save-restore/src/test/kotlin/ru/nsu/fit/reflex/project_management/save_restore/CoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.nsu.fit.reflex.project_management.save_restore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
